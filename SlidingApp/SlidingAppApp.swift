a675061e7af55b02409465750eda254ea15677d9
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-04-05 14:39:26 +0530

    Initial Commit

---
+++ SlidingApp/SlidingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingAppApp.swift
+//  SlidingApp
+//
+//  Created by Bhumika Patel on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SlidingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
